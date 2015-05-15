6e9e05b837ff89d9348b38d757abac8a5aecd109
mrajagopal <rajagopal.madhu@gmail.com>
2015-05-15 16:26:35 +1200

    Update urlRewrite.js

--- urlRewrite.js
+++ urlRewrite.js
@@ -1,1 +1,29 @@
+"use strict";
 
+var vsm = require('lrs/virtualServerModule');
+var http = require('http');
+var url = require('url');
+var hostname = 'www.example.com'
+var 301_MOVED_PERMANENTLY = 301;
+
+function requestHandler(servReq, servResp, cliReq){
+  servReq.on('response', responseHandler(cliResp){
+    cliResp.bindHeaders(servResp);
+    if (servResp.statusCode === 301_MOVED_PERMANENTLY){
+      var location = servResp.getHeader('Location');
+      servResp.removeHeader('Location');
+      servResp.setHeader('Location', hostname+location);
+    }
+    
+    cliResp.pipe(servResp);
+  });
+  
+  var reqUrlparsed = url.parse(servReq.url);
+  servReq.removeHeader('Host');
+  servReq.addHeader('Host', reqUrlParsed.pathname);
+  cliReq();
+}
+
+vsm.on('exist', 'appsContentServer', function(vs){
+  vs.on('request', requestHandler);
+});
